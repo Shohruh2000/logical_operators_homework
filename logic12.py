230bfa9f975bd2e57e6bbc7834f7a8e93cbbd96e
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-26 08:30:46 +0500

    logic12.py

--- logic12.py
+++ logic12.py
@@ -6,7 +6,7 @@ def main(a):
     Returns:
         bool: answer
     """
-    return a//10 == a%10 and (9 < a and a < 99)
+    return (a//10 == a%10) and (9 < a and a < 99)
 
 answer  = main(22)
-print(answer)
+print(bool(answer))
