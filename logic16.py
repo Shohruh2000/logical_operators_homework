b2d9dfab05e6d6a18f564880d522f651ef76c350
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-26 08:26:26 +0500

    logic16.py

--- logic16.py
+++ logic16.py
@@ -6,7 +6,7 @@ def main(a):
     Returns:
         bool: answer
     """
-    return (a > 9999 and a < 1000000) and ( a%2 ==0 or a%2 == 1 )
+    return (a > 9999 and a < 100000) and ( a%2 ==0 or a%2 == 1 )
 
 answer = main(12356)
 print(answer)
