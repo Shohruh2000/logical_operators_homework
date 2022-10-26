8f384bd7a51beaee7a38230552d8beea3c37d890
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-26 21:34:19 +0500

    logic17.py

--- logic17.py
+++ logic17.py
@@ -1,3 +1,6 @@
+import re
+
+
 def main(a):
     """
     Given a five-digit integer a,  check the following statement "All digits of the number are in ascending order".
@@ -11,8 +14,11 @@ def main(a):
     x3 = (a//100)%10
     x4 = ((a//100)//10)%10
     x5 = a//10000
+    x1=a%10
 
-    return (x5==(x4+1) and x4==(x3+1) and x3== (1+x2)and x2==(x1+1)) and (9999<a and a<100000)
 
-answer = main(87654)
+    return (x5==(x4+1) and x4==(x3+1) and x3== (1+x2)and x2==(x1+1)) and (10000<=a and a<=99999)
+    return x1,x2,x3,x4,x5
+
+answer = main(543219)
 print(answer)
